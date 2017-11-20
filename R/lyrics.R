174ec3e5e569d038fd71e5cf1d9637660cb9475b
ewen_ <ewenhenderson@gmail.com>
2017-11-20 23:25:56 +0000

    add track section & artist cols in lyrics func

--- R/lyrics.R
+++ R/lyrics.R
@@ -3,7 +3,7 @@
 #' Get lyrics from Genius' lyric pages.
 #' @param lyrics_url lyric page URL (like in \code{lyrics_url} returned by \code{\link{search_track}})
 #' @examples
-#' get_lyrics(lyrics_url = "/Waka-flocka-flame-rooster-in-my-rari-lyrics")
+#' get_lyrics(lyrics_url = "/Mike-will-made-it-california-rari-lyrics")
 #' @export
 get_lyrics <- function(lyrics_url) {
 
@@ -28,11 +28,29 @@ get_lyrics <- function(lyrics_url) {
   lyrics$cues <- stringr::str_detect(lyrics$line, "\\[|\\]")
 
   # split string at colon
-  lyrics$cue_split <- stringr::str_split(lyrics$line[lyrics$cues == TRUE], ":")
+  lyrics$cue_split <- stringr::str_split(lyrics$line, ":")
 
-  #
+  # function to map track section
+  lyrics$track_section <- ifelse(lyrics$cues == TRUE, sapply(lyrics$cue_split, "[", 1), NA)
+
+  # function to map artist
+  lyrics$lyric_artist_name <- ifelse(lyrics$cues == TRUE, sapply(lyrics$cue_split, "[", 2), NA)
+
+  # remove brackets from strings
+  lyrics <- dplyr::mutate_at(lyrics, c("track_section", "lyric_artist_name"), gsub,
+                             pattern="\\[|\\]", replacement="")
+
+  # remove white space
+  lyrics <- dplyr::mutate_at(lyrics, c("track_section", "lyric_artist_name"), stringr::str_trim)
+
+  # replace NAs with section/artists
+  lyrics <- dplyr::mutate_at(lyrics, c("track_section", "lyric_artist_name"),
+                             zoo::na.locf)
+
+  # remove cue split col
+  lyrics$cue_split <- NULL
 
   # Remove lines with things such as [Intro: person & so and so]
-  return(lyrics[stringr::str_detect(lyrics$line, "\\[|\\]") == FALSE, ])
+  return(lyrics[lyrics$cues == FALSE, ])
 
 }

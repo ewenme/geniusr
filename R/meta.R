0fd6bbbc447f54c741544c99103aabec39923fb8
ewen_ <ewenhenderson@gmail.com>
2017-11-21 00:11:47 +0000

    base meta func

--- R/meta.R
+++ R/meta.R
@@ -10,4 +10,23 @@ track_meta <- function(track_id) {
   # extract request content
   res <- httr::content(req)
 
+  # drill down
+  res <- res$response
+
+  # extract track info from returned results
+  track_info <- purrr::map_df(1:length(res), function(x) {
+    trk <- res[[x]]
+    alb <- res[[x]]$album
+    art <- res[[x]]$primary_artist
+    list(
+      track_id = trk$id,
+      track_name = trk$title_with_featured,
+      lyrics_url = trk$path,
+      artist_id = art$id
+    )
+  })
+
+  # isolate unique pairs
+  return(unique(track_info))
+
 }

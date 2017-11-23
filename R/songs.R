a50c77283c5ca782d2412b0fbb5d86dca1db843f
ewen_ <ewenhenderson@gmail.com>
2017-11-23 00:46:01 +0000

    working song list func

--- R/songs.R
+++ R/songs.R
@@ -9,45 +9,26 @@ artist_songs <- function(artist_id) {
 
   while (i > 0) {
   # search for artist
-  req <- httr::GET(url = paste0(base_url, artist_id, "/songs", '&per_page=', 50, '&page=', i),
+  req <- httr::GET(url = paste0(base_url, artist_id, "/songs", '?per_page=', 50, '&page=', i),
                    httr::add_headers(Authorization=paste0("Bearer ", genius_token())))
 
-  req <- httr::GET(url = paste0(base_url, artist_id, "/songs", '&per_page=', 10, '&page=', 1),
-                   httr::add_headers(Authorization=paste0("Bearer ", genius_token())))
+  # req <- httr::GET(url = paste0(base_url, artist_id, "/songs", '?per_page=', 10, '&page=', 1),
+  #                  httr::add_headers(Authorization=paste0("Bearer ", genius_token())))
 
   # extract request content
-  res <- httr::content(req)
+  req <- httr::content(req)
 
   # drill down
-  res <- res$response
+  req <- req$response
 
-  track_lyric_urls <- c(track_lyric_urls, res$songs)
+  track_lyric_urls <- c(track_lyric_urls, req$songs)
 
-  if (!is.null(res$next_page)) {
-    i <- res$next_page
+  if (!is.null(req$next_page)) {
+    i <- req$next_page
   } else {
     break
   }
 
   }
 
-  # extract request content
-  res <- httr::content(req)
-
-  # drill down
-  res <- res$response
-
-}
-
-
-track_lyric_urls <- list()
-i <- 1
-while (i > 0) {
-  tmp <- GET(requestURL, query = list(access_token = token, per_page = 50, page = i)) %>% content %>% .$response
-  track_lyric_urls <- c(track_lyric_urls, tmp$songs)
-  if (!is.null(tmp$next_page)) {
-    i <- tmp$next_page
-  } else {
-    break
-  }
 }

5a66bbd9766a2d69554ef7011e3fc04b45a305b7
ewen_ <ewenhenderson@gmail.com>
2020-04-13 12:06:05 +0100

    fix tests, add codemeta

--- tests/testthat/test-tidy_album.R
+++ tests/testthat/test-tidy_album.R
@@ -1,12 +1,13 @@
 context("tidy_album_xxx")
 
 id <- 337082
-album <- get_album(album_id = id)
 
 test_that("tidy_album_performances returns the right output formats", {
 
   skip_on_cran()
   skip_if_offline()
+
+  album <- get_album(album_id = id)
   output <- tidy_album_performances(album)
   expect_is(output, "tbl_df")
   expect_is(output, "tbl")
--- tests/testthat/test-tidy_song.R
+++ tests/testthat/test-tidy_song.R
@@ -1,12 +1,14 @@
 context("tidy_song_xxx")
 
 id <- 3039923
-song <- get_song(song_id = id)
 
 test_that("tidy_song_relationships returns the right output formats", {
 
   skip_on_cran()
   skip_if_offline()
+
+  song <- get_song(song_id = id)
+
   output <- tidy_song_relationships(song)
   expect_is(output, "tbl_df")
   expect_is(output, "tbl")
@@ -18,6 +20,9 @@ test_that("tidy_song_performances returns the right output formats", {
 
   skip_on_cran()
   skip_if_offline()
+
+  song <- get_song(song_id = id)
+
   output <- tidy_song_performances(song)
   expect_is(output, "tbl_df")
   expect_is(output, "tbl")
